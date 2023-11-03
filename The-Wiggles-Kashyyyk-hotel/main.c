1fdf5a7ebd391b164876c7c5f42bf0868d1dbb95
Zǎoshang hǎo zhōngguó xiànzài wǒ yǒu BING CHILLING wǒ hěn xǐhuān BING CHILLING dànshì sùdù yǔ jīqíng 9 bǐ BING CHILLING sùdù yǔ jīqíng sùdù yǔ jīqíng 9 wǒ zuì xǐhuān suǒyǐ…xiànzài shì yīnyuè shíjiān zhǔnbèi 1 2 3 liǎng gè lǐbài yǐhòu sùdù yǔ jīqíng 9 ×3 <jonah.wilsontroy23@students.bhasvic.ac.uk>
2023-11-03 01:27:39 +0000

    final laptop

--- The-Wiggles-Kashyyyk-hotel/main.c
+++ The-Wiggles-Kashyyyk-hotel/main.c
@@ -1,40 +1,63 @@
+//notes section:
+//ben will try to break out code, so make sure to add validation checks!
+//need to have the profile data stored on the computer itself.
+//checkout will use temp variables and update global on finish, while the others can use global variables in the funcs.
+
 #include <stdio.h>
 #include <string.h>
 
-//struct area (this is just to get all 6 profiles to work individually)
-struct profile {
-    char name[64];
-    char surname[64];
-    int childGuestCount;
-    int adultGuestCount;
-    int boardType;
-};
+#define maxStringVariables 2
+#define maxIntegerVariables 6
+int userCharArray[6][maxStringVariables][64];
+int userIntArray[6][maxIntegerVariables];
+
+//variable dictionary:
+//userCharArray[profile][which var][which letter of string]'s variables:
+// name -> [0]
+// surname -> [1]
+// bookingID -> [2]
+// 
+
+//userIntArray[profile][which var]'s   variables:
+// child guest count -> [0]
+// adult guest count -> [1]
+// length of stay -> [2]
+// board type -> [3]
+// daily newspaper option -> [4]
+// Date of birth -> [5]
+// 
+
 
 //function area
-char checkIn() {
+int checkIn(int roomsOccupied) {
     //ed
-    char userCharArray[which var][which letter of string] //for this decleration, enter max possible array depth/length
-    int userIntArray[each term has the variables value] //enter max int variables used for this decleration :0
+    // Key: userCharArray[profile][which var][which letter of string]
+    // Key: userIntArray[profile][variable]
 
-    //at any point, an input of 0 or e (your choice) will exit the function by using "return 0"
+    //at any point, an input of 0 or e (your choice) will exit the function by using "return roomsOccupied"
 
     //task list:
     //exit immediately if all rooms are full.
-    //
+    //take name, and surname  as char array inputs
+    //take child guests, adult guests, length of stay, board type, daily newspaper option, and Date of birth (in days since year 0)  as integer array inputs
+    //display room options
+    //take room choice input
+    //update room info and profile info.
 
-
-    return userCharArray and the userIntArray somehow..
+    roomsOccupied++; //this is an easy way to check how many rooms are used. you can replace this with a different system if you'd like.
+    return(roomsOccupied);
 }
 
-char checkOut() {
+void checkOut() {
     //christian
-    char userCharArray[which var][which letter of string] //for this decleration, enter max possible array depth/length
-    int userIntArray[each term has the variables value] //enter max int variables used for this decleration :0
+    // Key: userCharArray[profile][which var][which letter of string]
+    // Key: userIntArray[profile][variable]
 
-    //at any point, an input of 0 or e (your choice) will exit the function by using "return 0"
+    //at any point, an input of 0 or e (your choice) will exit the function by using "return 0
 
 
     //task list:
+    //figure out which profile to check out by asking for bookingID, and checking for matches
     //calculate total cost of room
     // calculate  total cost of parties board (meal)
     //if had daily newspapers add 5.50
@@ -43,21 +66,17 @@ char checkOut() {
 
     //show users booing id and main guests name
     //each of the subtotals amounts frm amove where it is tleast >0
-    //overall bill
-
-    //
+    //display overall bill
+    //update room occupancy and profile info.
 
     //nice job!
 
-
-
-    return userCharArray and the userIntArray somehow..
 }
 
-char bookTable() {
+void bookTable() {
     //jonah
-    char userCharArray[which var][which letter of string] //for this decleration, enter max possible array depth/length
-    int userIntArray[each term has the variables value] //enter max int variables used for this decleration :0
+    // Key: userCharArray[profile][which var][which letter of string]
+    // Key: userIntArray[profile][variable]
 
     //at any point, an input of 0 or e (your choice) will exit the function by using "return 0"
 
@@ -65,58 +84,52 @@ char bookTable() {
     //task list:
     // store tables: tatooine, endor, naboo
 
-    return userCharArray and the userIntArray somehow..
 }
 
 //main
 int main() {
     //declaring variables and other shiz
     char choice = 'x';
+    int hi = 0;
+    int roomsOccupied = 0;
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 5; j++) {
+            userIntArray[i][j] = j;
+        }
+    }
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 2; j++) {
+            for (int k = 0; k < 64; k++) {
+                userCharArray[i][j][k] = k;
+            }
+        }
+    }
 
-    //
-    //hotel program loop
-    while (1) {
-        printf("\nChoose a function (i to check in, o to check out, or b to book a table): ");
-        scanf("%c", &choice);
-
+    while (1) {           // choice loop
+        printf("\nChoose a function (i to check in, o to check out, or b to book a table):\n");
+        scanf("%c ", &choice);
         if (choice == 'i') {          // check in
             //ed
-            printf("\n>Checking in...");
-            checkIn();
-            //set each char array term in the desired profile to the function's
-            //set each int array term in the desired profile to the function's
-
-        } else if (choice == 'o') {   // check out
+            printf(">Checking in...\n");
+            roomsOccupied = checkIn(roomsOccupied);
+        }
+        else if (choice == 'o') {   // check out
             //christian
             printf("\n>Checking out...");
             checkOut();
-            //set each char array term in the desired profile to the function's
-            //set each int array term in the desired profile to the function's
-
-        } else if (choice == 'b') {   // booking a table
+        }
+        else if (choice == 'b') {   // booking a table
             //jonah
             printf("\n>Booking in...");
             bookTable();
-            //set each char array term in the desired profile to the function's
-            //set each int array term in the desired profile to the function's
-
-        } else {                   //invalid input
+        }
+        else {                   //invalid input
             printf("\n>Invalid Input. Try again.");
         }
-
-
-
         //this is the end of while loop
     }
     return 0;
 }
-
-//notes section:
-//ben will try to break out code, so make sure to add validation checks!
-//need to have the profile data stored on the computer itself.
-//checkout will use temp variables and update global on finish, while the others can use global variables in the funcs.
-
-
 //test
 //hello there ed and christian
 // hello there
@@ -127,11 +140,3 @@ int main() {
 //chicken fingers
 // // //
 //
-
-int main2() {   // for testing purposes
-    char userProfile_char[which profile][which var][stores th rest strings];
-    int userProfile_int[profiles][which variable];
-    char tables[][]
-
-    return 0;
-}

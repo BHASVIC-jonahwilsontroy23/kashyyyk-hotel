433b51cf240c84386662b824f8e29bc54d8529d0
Zǎoshang hǎo zhōngguó xiànzài wǒ yǒu BING CHILLING wǒ hěn xǐhuān BING CHILLING dànshì sùdù yǔ jīqíng 9 bǐ BING CHILLING sùdù yǔ jīqíng sùdù yǔ jīqíng 9 wǒ zuì xǐhuān suǒyǐ…xiànzài shì yīnyuè shíjiān zhǔnbèi 1 2 3 liǎng gè lǐbài yǐhòu sùdù yǔ jīqíng 9 ×3 <jonah.wilsontroy23@students.bhasvic.ac.uk>
2023-11-04 16:10:26 +0000

    saturfay

--- The-Wiggles-Kashyyyk-hotel/main.c
+++ The-Wiggles-Kashyyyk-hotel/main.c
@@ -5,14 +5,16 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
-#define maxStringVariables 2
+
+#define maxStringVariables 3
 #define maxIntegerVariables 6
-int userCharArray[6][maxStringVariables][64];
+char userCharArray[6][maxStringVariables][64];
 int userIntArray[6][maxIntegerVariables];
 
 //variable dictionary:
-//userCharArray[profile][which var][which letter of string]'s variables:
+//userCharArray[profile][which var]'s variables:
 // name -> [0]
 // surname -> [1]
 // bookingID -> [2]
@@ -22,19 +24,39 @@ int userIntArray[6][maxIntegerVariables];
 // child guest count -> [0]
 // adult guest count -> [1]
 // length of stay -> [2]
-// board type -> [3]
+// board type (FB=0, HB=1, BB=2)-> [3]
 // daily newspaper option -> [4]
 // Date of birth -> [5]
-// 
+//
+int x=0
+void ArraychildGC(int profile){
+    printf("how many children are included in your booking?");
+    fflush(stdin);
+    scanf("%d", &userIntArray[profile][0]);
+}
+void ArrayAdultGC(int profile){
+    printf("how many adults are in your booking?");
+    fflush(stdin);
+    scanf("%d",&userIntArray[profile][1]);
+}
+void ArrayLengthOS(int profile){
+    printf("how long are you staying for?");
+    fflush(stdin);
+    scanf("%d", &userIntArray[profile][2]);
+}
+void BoardType(int profile) {
+    printf("what tyoe of stay are you having? please enter BB/FB/HB");
+    fflush(stdin);
 
 
+}
 //function area
 int checkIn(int roomsOccupied) {
     //ed
     // Key: userCharArray[profile][which var][which letter of string]
     // Key: userIntArray[profile][variable]
 
-    //at any point, an input of 0 or e (your choice) will exit the function by using "return roomsOccupied"
+    //at any point, an input of 0 or e (your choice) will clear and exits the function by using "return roomsOccupied"
 
     //task list:
     //exit immediately if all rooms are full.
@@ -43,6 +65,10 @@ int checkIn(int roomsOccupied) {
     //display room options
     //take room choice input
     //update room info and profile info.
+    int profile = roomsOccupied;
+    ArraychildGC(profile);
+
+    //booking id ranodm number will be 4 digits long
 
     roomsOccupied++; //this is an easy way to check how many rooms are used. you can replace this with a different system if you'd like.
     return(roomsOccupied);
@@ -56,8 +82,65 @@ void checkOut() {
     //at any point, an input of 0 or e (your choice) will exit the function by using "return 0
 
 
+    char BookingID = 0;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int namePos = 0;
+    int sNamePos = 0;
+    int idPos = 0;
+    bool idFound = false;
+
+
+
+
     //task list:
     //figure out which profile to check out by asking for bookingID, and checking for matches
+
+
+
+
+    while (idFound == false)
+    {
+
+
+        printf("Enter Booking ID");
+        scanf("%d",&BookingID);
+        // ill sort this bookingID inputting section
+
+
+
+        for (i=0;i<6;i++) {
+            for (j=0;j<6;j++) {
+
+                if (userCharArray[i][j] == BookingID)
+                {
+                    namePos = i;
+                    sNamePos = j;
+                    idPos = k;
+                    idFound = true;
+                }
+            }
+        }
+
+        if (idFound = false)
+    }
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
     //calculate total cost of room
     // calculate  total cost of parties board (meal)
     //if had daily newspapers add 5.50
@@ -71,10 +154,24 @@ void checkOut() {
 
     //nice job!
 
+    if (userIntArray[profile][4] == 1) {
+
 }
 
 void bookTable() {
     //jonah
+
+    for (int i=0;i<6;i++) {
+        char enteredBookingID[68];
+        int profile;
+        printf("Enter bookingID: ")
+        scanf("%s", enteredBookingID);
+        strcpy(userCharArray[2][2],"Smith9999");
+        printf("\n [2][2] = %s",userCharArray[2][2]);
+        if (strcmp(enteredBookingID, userCharArray[i][2])) {
+            profile = i;
+    }
+        printf("\n i = %d",profile);
     // Key: userCharArray[profile][which var][which letter of string]
     // Key: userIntArray[profile][variable]
 
@@ -82,7 +179,11 @@ void bookTable() {
 
 
     //task list:
-    // store tables: tatooine, endor, naboo
+    // store tables: tatooine [0], endor [1], naboo [2] -> int tables[3]
+    //take and validate booking id
+    // if (userIntArray[profile][3] == 2) then exit
+    //
+
 
 }
 
@@ -90,7 +191,6 @@ void bookTable() {
 int main() {
     //declaring variables and other shiz
     char choice = 'x';
-    int hi = 0;
     int roomsOccupied = 0;
     for (int i = 0; i < 6; i++) {
         for (int j = 0; j < 5; j++) {
@@ -107,7 +207,7 @@ int main() {
 
     while (1) {           // choice loop
         printf("\nChoose a function (i to check in, o to check out, or b to book a table):\n");
-        scanf("%c ", &choice);
+        scanf(" %c", &choice);
         if (choice == 'i') {          // check in
             //ed
             printf(">Checking in...\n");
---
+++ jonah-testing/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+//int main() {
+//    while (1) {
+//        printf("Hello, World!\n");
+//        char mystr1[2][5] = {"hello"};
+//        char mystr2[2][5] = {"", "bells"};
+//        printf("Input1:\n");
+//        scanf("%s", mystr1[0]);
+//        printf("Input2:\n");
+//        scanf("%s", mystr2[1]);
+//
+//        printf("1: %s\n2: %s\n", mystr1[0], mystr2[1]);
+//        if (strcmp(mystr1[0], mystr2[1])) {
+//            printf("scess\n");
+//        }
+//    }
+//    return 0;
+//}
+
+int main() {
+    char var1 = 69;
+    int var2 = 10;
+    printf("char: %c or %d. combined: %d", var1, var1, var1+var2);
+    return 0;
+}
